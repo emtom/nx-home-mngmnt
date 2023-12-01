cd0340469b85d573a14b87e596b8a1566e65bfe5
emtom <malysz.tomasz@gmail.com>
2023-12-01 23:15:49 +0100

    initial commit

---
+++ apps/nx-home-fuels/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { UiModule } from '@nx-home-mngmnt/ui';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule, UiModule],
+  selector: 'nx-home-mngmnt-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'nx-home-fuels';
+}
---
+++ libs/shared/ui/src/lib/components/button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-home-mngmnt-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {}
