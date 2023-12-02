ae91f5e6ceb2fc02d4d05d27972a188413c8476e
emtom <malysz.tomasz@gmail.com>
2023-12-02 01:04:11 +0100

    feat: add initial material based shared ui components

---
+++ libs/shared/ui/src/lib/components/datepicker/datepicker.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/**
+ * CREATED using:
+ * cd libs/shared/ui/src/lib
+ * npx nx g @nx/angular:component datepicker --module=ui --directory=components/datepicker --standalone=false
+ * 
+ */
+
+@Component({
+  selector: 'nx-home-mngmnt-datepicker',
+  templateUrl: './datepicker.component.html',
+  styleUrl: './datepicker.component.scss',
+})
+export class DatepickerComponent {}
--- libs/shared/ui/src/lib/ui.module.ts
+++ libs/shared/ui/src/lib/ui.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './components/button.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+import { ButtonComponent } from './components/button/button.component';
+import { DatepickerComponent } from './components/datepicker/datepicker.component';
+
+/**
+ * TODO *
+ *  Split exported components to a separate modules
+ * */
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [ButtonComponent],
-  exports: [ButtonComponent]
+  imports: [CommonModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule],
+  declarations: [ButtonComponent, DatepickerComponent],
+  exports: [ButtonComponent, DatepickerComponent],
 })
 export class UiModule {}
